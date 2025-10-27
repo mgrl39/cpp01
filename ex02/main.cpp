82576a7fe5ad610a03776b619650c34eadec8652
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-10-27 18:00:33 +0100

    main

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,11 +6,25 @@
 /*   By: meghribe <meghribe@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 17:03:48 by meghribe          #+#    #+#             */
-/*   Updated: 2025/10/27 17:04:26 by meghribe         ###   ########.fr       */
+/*   Updated: 2025/10/27 17:30:11 by meghribe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(int argc, char *argv)
+#include <string>
+#include <iostream>
+
+int	main(int argc, char *argv[])
 {
+	std::string	string;
+
+	string = "HI THIS IS BRAIN";
+	std::string	*stringPTR;
+	std::string	&stringREF = string;
+
+	stringPTR = &string;
+
+	std::cout << &string << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
 	return ((void)argc, (void)argv, 0);
 }
