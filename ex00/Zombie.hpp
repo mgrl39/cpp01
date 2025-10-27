4e21872e1eb24f1ccaa58910440d24d5a8f77e98
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-10-27 15:32:42 +0100

    add headers and add newZombie, randomChump functions in class Zombie

--- ex00/Zombie.hpp
+++ ex00/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: meghribe <meghribe@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/26 15:20:37 by meghribe          #+#    #+#             */
-/*   Updated: 2025/10/27 15:08:55 by meghribe         ###   ########.fr       */
+/*   Updated: 2025/10/27 15:19:30 by meghribe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,11 @@ class Zombie {
 		
 	public:
 		Zombie();
+		~Zombie();
+		Zombie*		newZombie( std::string name );
+		void		randomChump( std::string name );
 		std::string	get_name(void);
-		void	announce(void);
+		void		announce(void);
 };
 
 #endif
