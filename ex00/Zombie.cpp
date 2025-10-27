cb093e39abd036d3197b3296af51bc2dea0d307f
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-10-27 16:40:31 +0100

    changing the delete from one site ot another

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: meghribe <meghribe@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/26 15:22:11 by meghribe          #+#    #+#             */
-/*   Updated: 2025/10/27 16:34:21 by meghribe         ###   ########.fr       */
+/*   Updated: 2025/10/27 16:40:02 by meghribe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,11 @@ Zombie::Zombie(std::string name)
 	this->name = name;
 }
 
+// TODO: idk if do the delete here or not.
 Zombie::~Zombie()
 {
-	std::cout << this->name << " : Destroyed..." << std::endl;
-	delete this;
+	std::cout << this->name << ": Destroyed..." << std::endl;
+	//delete this;
 }
 
 std::string	Zombie::get_name(void)
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: meghribe <meghribe@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 15:31:29 by meghribe          #+#    #+#             */
-/*   Updated: 2025/10/27 16:37:44 by meghribe         ###   ########.fr       */
+/*   Updated: 2025/10/27 16:39:23 by meghribe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ int	main(int argc, char *argv[])
 		(void)zombie;
 		zombie = zombie->newZombie("Pep");
 		zombie->announce();
+		delete zombie;
 
 		return ((void)argc, (void)argv, 0);
 }
