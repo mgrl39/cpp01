e8364a0e9462b21c69b61f1cbdb3fae9e5584c12
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-10-28 14:23:50 +0100

    i think i finished the main.cpp

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: meghribe <meghribe@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 17:03:48 by meghribe          #+#    #+#             */
-/*   Updated: 2025/10/27 17:30:11 by meghribe         ###   ########.fr       */
+/*   Updated: 2025/10/28 14:22:37 by meghribe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,18 @@ int	main(int argc, char *argv[])
 	std::string	&stringREF = string;
 
 	stringPTR = &string;
-
+	std::cout << "The memory address of the string variable:" << std::endl;
 	std::cout << &string << std::endl;
+	std::cout << "The memory address held by stringPTR:" << std::endl;
 	std::cout << stringPTR << std::endl;
+	std::cout << "The memory address held by stringREF:" << std::endl;
 	std::cout << &stringREF << std::endl;
-	return ((void)argc, (void)argv, 0);
+
+	std::cout << "The value of the string variable:" << std::endl;
+	std::cout << string << std::endl;
+	std::cout << "The value pointed to by stringPTR:" << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << "The value pointed to by stringREF:" << std::endl;
+	std::cout << stringREF << std::endl;
+	return (0);
 }
