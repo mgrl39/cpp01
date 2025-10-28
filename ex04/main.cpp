22156dadbad075d3da5d32c720abe3f54c60614e
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-10-28 18:37:28 +0100

    add ex03 and ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,23 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Weapon.hpp                                         :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: meghribe <meghribe@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/28 14:44:47 by meghribe          #+#    #+#             */
-/*   Updated: 2025/10/28 17:56:52 by meghribe         ###   ########.fr       */
+/*   Created: 2025/10/28 14:42:26 by meghribe          #+#    #+#             */
+/*   Updated: 2025/10/28 18:18:42 by meghribe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 #include <string>
 
-class Weapon
+static void	show_usage(char *argv)
 {
-	private:
-		std::string	type;
-	public:
-		std::string	get_type(void);
-		void		set_type(std::string type);
+	std::string	msg;
 
-};
+	msg = "takes filename and replaces every occurence of s1 with s2";
+	std::cout << "Usage:" << std::endl;
+	std::cout << argv << " filename s1 s2" << std::endl;
+	std::cout << msg << std::endl;
+}
+int	main(int argc, char *argv[])
+{
+	if (argc != 3)
+		show_usage(argv[0]);
+	return (0);
+}
+
